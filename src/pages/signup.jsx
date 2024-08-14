17921daceb7bc9041147e36db335cd33fcc3a39f
forevermarsh <marshmansur004@gmail.com>
2024-08-14 14:33:18 +0300

    reckless

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,45 +1,35 @@
 import "../css/signin.css";
 function SignUp() {
   return (
-    <main class="page-center">
-      <article class="sign-up">
-        <h2 class="sign-up__title">Go Walamin Go Green!</h2>
-        <p class="sign-up__subtitle">Sign in to your account to continue</p>
-        <form class="sign-up-form form" action="" method="">
-          <label class="form-label-wrapper">
-            <p class="form-label">Email</p>
-            <input
-              class="form-input"
-              type="email"
-              placeholder="Enter your email"
-              required
-            />
-          </label>
-          <label class="form-label-wrapper">
-            <p class="form-label">Password</p>
-            <input
-              class="form-input"
-              type="password"
-              placeholder="Enter your password"
-              required
-            />
-          </label>
-          <a class="link-info forget-link" href="##">
-            Forgot your password?
-          </a>
-          <label class="form-checkbox-wrapper">
-            <input class="form-checkbox" type="checkbox" required />
-            <span class="form-checkbox-label">Remember me next time</span>
-          </label>
-          <button
-            onclick="location.href='mainpage.html'"
-            class="form-btn primary-default-btn transparent-btn"
-          >
-            Sign in
-          </button>
-        </form>
-      </article>
-    </main>
+<main class="signup_page-center">
+  <article class="sign-up">
+    <h2 class="sign-up__title">Get started</h2>
+    <p class="sign-up__subtitle">Start creating the best possible user experience for you customers</p>
+    <form class="sign-up-form form" action="" method="">
+      <label class="form-label-wrapper">
+        <p class="form-label">Name</p>
+        <input class="form-input" type="text" placeholder="Enter your name" required />
+      </label>
+      <label class="form-label-wrapper">
+        <p class="form-label">Team</p>
+        <input class="form-input" type="email" placeholder="Enter your Team name" required />
+      </label>
+      <label class="form-label-wrapper">
+        <p class="form-label">Email</p>
+        <input class="form-input" type="email" placeholder="Enter your email" required />
+      </label>
+      <label class="form-label-wrapper">
+        <p class="form-label">Password</p>
+        <input class="form-input" type="password" placeholder="Enter your password" required />
+      </label>
+      <label class="form-checkbox-wrapper">
+        <input class="form-checkbox" type="checkbox" required />
+        <span class="form-checkbox-label">Remember me next time</span>
+      </label>
+      <button onclick="location.href='mainpage.html'" class="form-btn primary-default-btn transparent-btn">Sign up</button> 
+    </form>
+  </article>
+</main>
   );
 }
 
