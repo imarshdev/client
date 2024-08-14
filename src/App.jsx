986d056f4299c8fdf5b91e1fa675c2bc52fdfbb9
forevermarsh <marshmansur004@gmail.com>
2024-08-14 08:59:59 +0300

    make you mine

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.jsx'
-import './App.css'
+import { useState } from "react";
+import appLogo from "/favicon.svg";
+import PWABadge from "./PWABadge.jsx";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <img src={appLogo} className="logo" alt="Walamin logo" />
       </div>
-      <h1>Walamin</h1>
+      <h1>Walamin Green</h1>
       <div className="card">
         <p>
-          Go Walamin Go Green
+          Go Green Go Walamin
         </p>
       </div>
       <PWABadge />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
