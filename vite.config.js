ce8084ac82183e546d38bffbd533a216c942bfd5
forevermarsh <marshmansur004@gmail.com>
2024-08-14 08:51:35 +0300

    yess pleaseeee

--- vite.config.js
+++ vite.config.js
@@ -11,16 +11,30 @@ export default defineConfig({
     registerType: 'autoUpdate',
     injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
-
     manifest: {
       name: 'Walamin',
       short_name: 'Walamin',
-      description: 'Walamin',
+      description: 'Walamin Green Solutions Limited',
       theme_color: '#ffffff',
+
+      icons: [{
+        src: 'pwa-64x64.png',
+        sizes: '64x64',
+        type: 'image/png',
+      }, {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      }, {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      }, {
+        src: 'maskable-icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      }],
     },
 
     injectManifest: {
