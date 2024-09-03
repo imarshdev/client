51a94156adf39030bb05bfe7ec43214e9c483e7e
forevermarsh <marshmansur004@gmail.com>
2024-09-03 10:53:58 +0300

    comit

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -64,7 +64,7 @@ function Home() {
             </TouchableOpacity>
           </div>
           <p>
-            {time} {user.userName}
+            {time} {user.firstName}
           </p>
           <div>
             <span>{formattedTime}</span>
