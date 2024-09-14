abed73bb6eb95435c945dbd630067780c5c65dfc
forevermarsh <marshmansur004@gmail.com>
2024-09-14 15:25:35 +0300

    jhdshjvs

--- src/locationTracker.jsx
+++ src/locationTracker.jsx
@@ -1,49 +1,45 @@
 import { useContext, useEffect, useState } from "react";
+import axios from "axios";
 import { UserContext } from "../UserContext";
 
 export default function LocationTracker() {
   const { user } = useContext(UserContext);
-  const [token, setToken] = useState();
-  const [username, setUsername] = useState();
   const [longitude, setLongitude] = useState();
   const [latitude, setLatitude] = useState();
+
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const lng = position.coords.longitude;
-      const lat = position.coords.latitude;
-      setUsername(user.Username);
-      setToken(user.Token);
-      setLatitude(lat);
-      setLongitude(lng);
-      console.log(username);
-      console.log(token);
-      console.log(latitude);
-      console.log(longitude);
-    });
-    const sendLocations = async () => {
-      try {
-        const response = await axios.post(
-          "https://walamin-server.onrender.com/location",
-          {
-            username,
-            token,
-            latitude,
-            longitude,
+    if (!user) {
+      console.error("User not found in context");
+      return;
+    }
+
+    const getLocation = async () => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lng = position.coords.longitude;
+        const lat = position.coords.latitude;
+        setLongitude(lng);
+        setLatitude(lat);
+
+        const sendLocations = async () => {
+          try {
+            const response = await axios.post("(link unavailable)", {
+              username: user.Username,
+              token: user.Token,
+              latitude: lat,
+              longitude: lng,
+            });
+            console.log(response.data);
+          } catch (error) {
+            console.error(error);
           }
-        );
-        console.log("success");
-      } catch (error) {
-        if (error.response) {
-          setMessage(error.response.data.message);
-        } else {
-          setResult(3);
-          setMessage(
-            "An error occurred, Please try again."
-          );
-        }
-      }
+        };
+
+        sendLocations();
+      });
     };
-    sendLocations()
-  });
+
+    getLocation();
+  }, [user]);
+
   return null;
 }
