c1990cbff31428b3b376172f80b7c3f905fc4092
forevermarsh <marshmansur004@gmail.com>
2024-09-16 13:10:05 +0300

    byr

--- src/pages/allloactions.jsx
+++ src/pages/allloactions.jsx
@@ -1,185 +1,124 @@
 import { useEffect, useState, useRef } from "react";
 import { TouchableOpacity } from "react-native-web";
+import { Link } from "react-router-dom";
 import { BottomSheet } from "react-spring-bottom-sheet";
+import ride from "../assets/ride.svg";
+import delivery from "../assets/delivery.svg";
+import schedule from "../assets/schedule.svg";
+import dine from "../assets/dine.svg";
+import send from "../assets/send.svg";
+import withdraw from "../assets/withdraw.svg";
+import shopping from "../assets/shopping.svg";
+import { RiAuctionLine } from "react-icons/ri";
+import { FaServicestack } from "react-icons/fa";
 
 const AllLocations = () => {
-  const [open, setOpen] = useState(false);
-  const dismis = () => {
-    setOpen(false);
-  };
+
   return (
-    <div style={{ width: "100vw" }}>
-      <div id="mapelement" style={{ height: "55vh", width: "100%" }} />
-      <div
-        style={{
-          width: "100%",
-          height: "45vh",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "start",
-          justifyContent: "center",
-          boxSizing: "border-box",
-          padding: "20px",
-          boxShadow: "0 1px 20px rgba(160, 163, 189, 0.801)",
-          borderRadius: "10px 10px 0 0 ",
-        }}
-      >
-        <h2>Username: Marsh Mansur</h2>
-        <>
-          <p style={{ fontSize: "14px" }}>Origin: </p>
-          <span>9J28+Q3G, Ntinda - Kisaasi Rd, Kampala, Uganda</span>
-        </>
-        <>
-          <p style={{ fontSize: "14px" }}>Destination: </p>
-          <span>Kyaliwajjala Trading Center</span>
-        </>
-        <>
-          <p style={{ fontSize: "14px" }}>Cost: </p>
-          <span>Shs. 4500</span>
-        </>
-        <br />
-        <div
-          style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          <TouchableOpacity
-            style={{
-              width: "45%",
-              height: "3rem",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              backgroundColor: "limegreen",
-              borderRadius: "5px",
-            }}
-          >
-            <span style={{ color: "#fff" }}>call user</span>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              width: "45%",
-              height: "3rem",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              backgroundColor: "#fff",
-              borderRadius: "5px",
-              border: "solid 0.1px limegreen",
-            }}
-            onPress={() => setOpen(true)}
-          >
-            <span>Start Ride</span>
-          </TouchableOpacity>
-        </div>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        boxSizing: "border-box",
+        padding: "10px",
+        alignItems: "center",
+      }}
+    >
+      <div className="mid_details_upper">
+        <TouchableOpacity id="service_item">
+          <Link to="/map2" id="link_item">
+            <img src={delivery} style={{ width: "36px" }} />
+            <p style={{ fontSize: 12 }}>Delivery</p>{" "}
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity id="service_item">
+          <Link to="/currentRide" id="link_item">
+            <img src={ride} style={{ width: "34px" }} />
+            <p style={{ fontSize: 12 }}>Order Ride</p>
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity id="service_item">
+          <Link to="/agree" id="link_item">
+            <img src={schedule} style={{ width: "34px" }} />
+            <p style={{ fontSize: 12 }}>Schedule ride</p>{" "}
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity id="service_item">
+          <Link id="link_item">
+            <img src={dine} style={{ width: "34px" }} />
+            <p style={{ fontSize: 12 }}>Dine</p>{" "}
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity id="service_item">
+          <Link id="link_item">
+            <img src={shopping} style={{ width: "34px" }} />
+            <p style={{ fontSize: 12 }}>Shopping</p>{" "}
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity id="service_item">
+          <Link id="link_item">
+            <RiAuctionLine size={34} color="limegreen" />
+            <p style={{ fontSize: 12 }}>Auction House</p>{" "}
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity id="service_item">
+          <Link id="link_item">
+            <img src={send} style={{ width: "34px" }} />
+            <p style={{ fontSize: 12 }}>Send Money</p>{" "}
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity id="service_item">
+          <Link id="link_item">
+            <img src={withdraw} style={{ width: "34px" }} />
+            <p style={{ fontSize: 12 }}>Withdraw</p>{" "}
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity id="service_item">
+          <Link id="link_item">
+            <img src={send} style={{ width: "34px" }} />
+            <p style={{ fontSize: 12 }}>Send Money</p>{" "}
+          </Link>
+        </TouchableOpacity>
+
+        <TouchableOpacity id="service_item">
+          <Link id="link_item">
+            <img src={send} style={{ width: "34px" }} />
+            <p style={{ fontSize: 12 }}>Send Money</p>{" "}
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity id="service_item">
+          <Link id="link_item">
+            <img src={send} style={{ width: "34px" }} />
+            <p style={{ fontSize: 12 }}>Send Money</p>{" "}
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity id="service_item">
+          <Link id="link_item">
+            <img src={send} style={{ width: "34px" }} />
+            <p style={{ fontSize: 12 }}>Send Money</p>{" "}
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity id="service_item">
+          <Link id="link_item">
+            <img src={send} style={{ width: "34px" }} />
+            <p style={{ fontSize: 12 }}>Send Money</p>{" "}
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity id="service_item">
+          <Link id="link_item">
+            <img src={send} style={{ width: "34px" }} />
+            <p style={{ fontSize: 12 }}>Send Money</p>{" "}
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity id="service_item">
+          <Link id="link_item">
+            <img src={send} style={{ width: "34px" }} />
+            <p style={{ fontSize: 12 }}>Send Money</p>{" "}
+          </Link>
+        </TouchableOpacity>
       </div>
-      <BottomSheet
-        open={open}
-        snapPoints={({ maxHeight }) => [maxHeight]}
-        onDismiss={dismis}
-        header={<p>On Trip</p>}
-      >
-        <div
-          style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column",
-          }}
-        >
-          <p>Ongoing</p>
-          <Ongoing setOpen={setOpen} open={open} />
-        </div>
-      </BottomSheet>
     </div>
   );
 };
 
 export default AllLocations;
-
-const Ongoing = ({ setOpen, open }) => {
-  const [map, setMap] = useState(null);
-  const mapContainerRef = useRef(null);
-  useEffect(() => {
-    if (!mapContainerRef.current) return;
-
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: "mapbox://styles/mapbox/streets-v11?logo=false",
-      zoom: 15,
-    });
-
-    setMap(map);
-
-    // Add marker for current location
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const longitude = position.coords.longitude;
-        const latitude = position.coords.latitude;
-        map.setCenter([longitude, latitude]);
-        map.setZoom(15);
-        const marker = new mapboxgl.Marker()
-          .setLngLat([longitude, latitude])
-          .addTo(map);
-      },
-      (error) => {
-        console.error("Error getting location:", error);
-      },
-      { enableHighAccuracy: true }
-    );
-
-    return () => {
-      map.remove();
-    };
-  }, []);
-  return (
-    <>
-      <div
-        ref={mapContainerRef}
-        id="map"
-        style={{ width: "100%", height: "60vh" }}
-      ></div>
-      <br />
-      <br />
-      <div
-        style={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <TouchableOpacity
-          style={{
-            width: "45%",
-            height: "3rem",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#fff",
-            borderRadius: "5px",
-            border: "solid 0.1px limegreen",
-          }}
-        >
-          <span>Add Stop</span>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{
-            width: "45%",
-            height: "3rem",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "limegreen",
-            borderRadius: "5px",
-          }}
-          onPress={() => setOpen(false)}
-        >
-          <span style={{ color: "#fff" }}>End Ride</span>
-        </TouchableOpacity>
-      </div>
-    </>
-  );
-};
