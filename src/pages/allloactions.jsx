2727b6e26f693c711375469f0fe410f8b26c4a57
forevermarsh <marshmansur004@gmail.com>
2024-09-16 10:21:36 +0300

    yesss sirrrrr

--- src/pages/allloactions.jsx
+++ src/pages/allloactions.jsx
@@ -1,36 +1,185 @@
+import { useEffect, useState, useRef } from "react";
+import { TouchableOpacity } from "react-native-web";
+import { BottomSheet } from "react-spring-bottom-sheet";
 
 const AllLocations = () => {
+  const [open, setOpen] = useState(false);
+  const dismis = () => {
+    setOpen(false);
+  };
   return (
     <div style={{ width: "100vw" }}>
-      <div id="mapElement" style={{ height: "50vh", width: "100%" }} />
+      <div id="mapelement" style={{ height: "55vh", width: "100%" }} />
       <div
         style={{
           width: "100%",
-          height: "50vh",
+          height: "45vh",
           display: "flex",
           flexDirection: "column",
-          alignItems: "center",
+          alignItems: "start",
           justifyContent: "center",
           boxSizing: "border-box",
           padding: "20px",
+          boxShadow: "0 1px 20px rgba(160, 163, 189, 0.801)",
+          borderRadius: "10px 10px 0 0 ",
         }}
       >
-        <h2>Ride Details</h2>
-        <label>
-          <p>Origin: </p>
+        <h2>Username: Marsh Mansur</h2>
+        <>
+          <p style={{ fontSize: "14px" }}>Origin: </p>
           <span>9J28+Q3G, Ntinda - Kisaasi Rd, Kampala, Uganda</span>
-        </label>
-        <label>
-          <p>Destination: </p>
+        </>
+        <>
+          <p style={{ fontSize: "14px" }}>Destination: </p>
           <span>Kyaliwajjala Trading Center</span>
-        </label>
-        <label>
-          <p>Cost: </p>
-          <span></span>
-        </label>
+        </>
+        <>
+          <p style={{ fontSize: "14px" }}>Cost: </p>
+          <span>Shs. 4500</span>
+        </>
+        <br />
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              width: "45%",
+              height: "3rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "limegreen",
+              borderRadius: "5px",
+            }}
+          >
+            <span style={{ color: "#fff" }}>call user</span>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              width: "45%",
+              height: "3rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#fff",
+              borderRadius: "5px",
+              border: "solid 0.1px limegreen",
+            }}
+            onPress={() => setOpen(true)}
+          >
+            <span>Start Ride</span>
+          </TouchableOpacity>
+        </div>
       </div>
+      <BottomSheet
+        open={open}
+        snapPoints={({ maxHeight }) => [maxHeight]}
+        onDismiss={dismis}
+        header={<p>On Trip</p>}
+      >
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <p>Ongoing</p>
+          <Ongoing setOpen={setOpen} open={open} />
+        </div>
+      </BottomSheet>
     </div>
   );
 };
 
 export default AllLocations;
+
+const Ongoing = ({ setOpen, open }) => {
+  const [map, setMap] = useState(null);
+  const mapContainerRef = useRef(null);
+  useEffect(() => {
+    if (!mapContainerRef.current) return;
+
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/streets-v11?logo=false",
+      zoom: 15,
+    });
+
+    setMap(map);
+
+    // Add marker for current location
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const longitude = position.coords.longitude;
+        const latitude = position.coords.latitude;
+        map.setCenter([longitude, latitude]);
+        map.setZoom(15);
+        const marker = new mapboxgl.Marker()
+          .setLngLat([longitude, latitude])
+          .addTo(map);
+      },
+      (error) => {
+        console.error("Error getting location:", error);
+      },
+      { enableHighAccuracy: true }
+    );
+
+    return () => {
+      map.remove();
+    };
+  }, []);
+  return (
+    <>
+      <div
+        ref={mapContainerRef}
+        id="map"
+        style={{ width: "100%", height: "60vh" }}
+      ></div>
+      <br />
+      <br />
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            width: "45%",
+            height: "3rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#fff",
+            borderRadius: "5px",
+            border: "solid 0.1px limegreen",
+          }}
+        >
+          <span>Add Stop</span>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            width: "45%",
+            height: "3rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "limegreen",
+            borderRadius: "5px",
+          }}
+          onPress={() => setOpen(false)}
+        >
+          <span style={{ color: "#fff" }}>End Ride</span>
+        </TouchableOpacity>
+      </div>
+    </>
+  );
+};
--- src/pages/captains.jsx
+++ src/pages/captains.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { ScrollView, TouchableOpacity } from "react-native-web";
 import ride from "../assets/ride.svg";
 import delivery from "../assets/delivery.svg";
@@ -9,6 +9,10 @@ import { MdAccessTime, MdStart } from "react-icons/md";
 import { IoCallOutline } from "react-icons/io5";
 import { IoIosArrowBack, IoMdPricetags } from "react-icons/io";
 import { useLocation, useNavigate } from "react-router-dom";
+import { BottomSheet } from "react-spring-bottom-sheet";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiaW1hcnNoIiwiYSI6ImNtMDZiZDB2azB4eDUyanM0YnVhN3FtZzYifQ.gU1K02oIfZLWJRGwnjGgCg";
 
 export default function CapDash() {
   const [declinedRides, setDeclinedRides] = useState({});
@@ -354,7 +358,10 @@ export function MapRide() {
   const [destinationLng, setDestinationLng] = useState({});
   const [userLat, setUserLat] = useState({});
   const [userLng, setUserLng] = useState({});
-
+  const [open, setOpen] = useState(false);
+  const dismis = () => {
+    setOpen(false);
+  };
   useEffect(() => {
     // Get user's current location
     navigator.geolocation.getCurrentPosition((position) => {
@@ -439,34 +446,177 @@ export function MapRide() {
     };
   });
   return (
-    <div style={{width: "100vw"}}>
-      <div id="mapElement" style={{ height: "50vh", width: "100%" }} />
+    <div style={{ width: "100vw" }}>
+      <div id="mapElement" style={{ height: "55vh", width: "100%" }} />
       <div
         style={{
           width: "100%",
-          height: "50vh",
+          height: "45vh",
           display: "flex",
           flexDirection: "column",
-          alignItems: "center",
+          alignItems: "start",
           justifyContent: "center",
-          boxSizing: 'border-box',
-          padding: '20px'
+          boxSizing: "border-box",
+          padding: "20px",
+          boxShadow: "0 1px 20px rgba(160, 163, 189, 0.801)",
+          borderRadius: "10px 10px 0 0 ",
         }}
       >
-        <h2>Ride Details</h2>
-        <label>
-          <p>Origin: </p>
+        <h2>Username: {rideData.username}</h2>
+        <>
+          <p style={{ fontSize: "14px" }}>Origin: </p>
           <span>{rideData.origin}</span>
-        </label>
-        <label>
-          <p>Destination: </p>
+        </>
+        <>
+          <p style={{ fontSize: "14px" }}>Destination: </p>
           <span>{rideData.destination}</span>
-        </label>
-        <label>
-          <p>Cost: </p>
-          <span>{rideData.cost}</span>
-        </label>
+        </>
+        <>
+          <p style={{ fontSize: "14px" }}>Cost: </p>
+          <span>Shs. {rideData.cost}</span>
+        </>
+        <br />
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              width: "45%",
+              height: "3rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "limegreen",
+              borderRadius: "5px",
+            }}
+            onPress={() => window.open(`tel:${rideData.contact}`, "_self")}
+          >
+            <span style={{ color: "#fff" }}>call user</span>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              width: "45%",
+              height: "3rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#fff",
+              borderRadius: "5px",
+              border: "solid 0.1px limegreen",
+            }}
+            onPress={() => setOpen(true)}
+          >
+            <span>Start Ride</span>
+          </TouchableOpacity>
+        </div>
       </div>
+      <BottomSheet
+        open={open}
+        snapPoints={({ maxHeight }) => [maxHeight]}
+        onDismiss={dismis}
+        header={<p>On Trip</p>}
+      >
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <p>Ongoing</p>
+          <Ongoing setOpen={setOpen} open={open} />
+        </div>
+      </BottomSheet>
     </div>
   );
 }
+
+const Ongoing = ({ setOpen, open }) => {
+  const [map, setMap] = useState(null);
+  const mapContainerRef = useRef(null);
+  useEffect(() => {
+    if (!mapContainerRef.current) return;
+
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/streets-v11?logo=false",
+      zoom: 15,
+    });
+
+    setMap(map);
+
+    // Add marker for current location
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const longitude = position.coords.longitude;
+        const latitude = position.coords.latitude;
+        map.setCenter([longitude, latitude]);
+        map.setZoom(15);
+        const marker = new mapboxgl.Marker()
+          .setLngLat([longitude, latitude])
+          .addTo(map);
+      },
+      (error) => {
+        console.error("Error getting location:", error);
+      },
+      { enableHighAccuracy: true }
+    );
+
+    return () => {
+      map.remove();
+    };
+  }, []);
+  return (
+    <>
+      <div
+        ref={mapContainerRef}
+        id="map"
+        style={{ width: "100%", height: "60vh" }}
+      ></div>
+      <br />
+      <br />
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            width: "45%",
+            height: "3rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#fff",
+            borderRadius: "5px",
+            border: "solid 0.1px limegreen",
+          }}
+        >
+          <span>Add Stop</span>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            width: "45%",
+            height: "3rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "limegreen",
+            borderRadius: "5px",
+          }}
+          onPress={() => setOpen(false)}
+        >
+          <span style={{ color: "#fff" }}>End Ride</span>
+        </TouchableOpacity>
+      </div>
+    </>
+  );
+};
