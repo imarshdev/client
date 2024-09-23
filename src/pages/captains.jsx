507d518646abb08f4e4100008cc258a7c860f6f6
forevermarsh <marshmansur004@gmail.com>
2024-09-23 13:39:52 +0300

    hey hey hey

--- src/pages/captains.jsx
+++ src/pages/captains.jsx
@@ -629,7 +629,7 @@ const Ongoing = ({ setOpen, open, destinationLat, destinationLng }) => {
     const directions = new window.MapboxDirections({
       accessToken: window.mapboxgl.accessToken,
       unit: "metric",
-      profile: "mapbox/walking",
+      profile: "mapbox/drivng",
       alternatives: false,
       geometries: "geojson",
       controls: { instructions: true, inputs: false },
@@ -674,7 +674,7 @@ const Ongoing = ({ setOpen, open, destinationLat, destinationLng }) => {
       <div
         ref={mapContainerRef}
         id="map"
-        style={{ width: "100%", height: "60vh" }}
+        style={{ width: "100%", height: "75vh" }}
       ></div>
       <br />
       <br />
